dce2b457841fb8355e9d978010dbf45692e54462
Master-Guy <master.guy.marcel@gmail.com>
2019-11-22 08:47:08 +0100

    First version

---
+++ InfinityBots/Main.cs
@@ -0,0 +1,41 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace InfiniteBots
+{
+    // captncraig, thanks for the initials code ;-)
+    public static class Main
+    {
+        public static UnityModManager.ModEntry mod;
+        
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            mod = modEntry;
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Worker),"InitStats"),
+                postfix: new HarmonyMethod(typeof(Main), nameof(Main.InitStats))
+            );
+
+            Type type = typeof(Worker);
+            FieldInfo info = type.GetField("m_LoseEnergy", BindingFlags.NonPublic | BindingFlags.Static);
+            info.SetValue(null, false);
+
+            return true;
+        }
+
+        [HarmonyPostfix]
+        static void InitStats()
+        {
+            Worker.m_AllHeadInfo.ForEach(delegate (WorkerHeadInfo whi)
+            {
+                whi.m_MaxInstructions = Int32.MaxValue;
+                whi.m_SerialPrefix = "IB";
+            });
+        }
+
+    }
+}
---
+++ InfinityBots/Readme.md
@@ -0,0 +1,8 @@
+# SuperBots
+
+I don't really think the arbitrary limits on bot capabilities are very fun. This mod cheats with the following effects:
+
+1. Bots do not lose energy and do not need recharging.
+2. Bots have much increased memory (64/256/1024kb).
+3. Bots have dramatically increased range.
+4. Bots all scan for items faster.
